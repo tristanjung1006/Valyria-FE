479cff853bb48921d6f450371b4a73c08c3e9e19
Daemon Jung <jjjssswww1006@gmail.com>
2024-05-07 15:15:38 +0900

    Chore MainActivity

--- app/src/main/java/com/daemon/hotd/ui/main/MainActivity.kt
+++ app/src/main/java/com/daemon/hotd/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.daemon.hotd
+package com.daemon.hotd.ui.main
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -18,18 +18,5 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 }
