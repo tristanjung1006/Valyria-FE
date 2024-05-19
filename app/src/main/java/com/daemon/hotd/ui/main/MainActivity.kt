acf065f79d14e0b2ba99a34ac4d9fa950cd7e7c4
Daemon Jung <jjjssswww1006@gmail.com>
2024-05-19 23:05:23 +0900

    Add shine effects

--- app/src/main/java/com/daemon/hotd/ui/main/MainActivity.kt
+++ app/src/main/java/com/daemon/hotd/ui/main/MainActivity.kt
@@ -1,22 +1,43 @@
 package com.daemon.hotd.ui.main
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import com.daemon.hotd.R
 import com.daemon.hotd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    lateinit var shine : View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        shine = findViewById(R.id.shine)
+        shineAnimation()
     }
+
+    private fun shineAnimation() {
+        val anim = AnimationUtils.loadAnimation(this, R.anim.left_right)
+        shine.startAnimation(anim)
+        anim.setAnimationListener(object : Animation.AnimationListener  {
+            override fun onAnimationStart(p0: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+                shine.startAnimation(anim)
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {
+
+            }
+
+
+        })
+    }
+
 }
